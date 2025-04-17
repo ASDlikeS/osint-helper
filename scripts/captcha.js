b2c0e634103457256571b2fce1f16ad59b4049a0
ASDlikeS <penetresing_test@tutamail.com>
2025-04-17 11:18:33 +0300

    fix: add captcha form in form tag, rewrite js script

--- scripts/captcha.js
+++ scripts/captcha.js
@@ -1,22 +1,23 @@
 const abc = "abcdef2ghi3j1klmn4opqr5tuvwx6yz";
-let txtSub;
-        let rndChar = "";
-        while (rndChar.length < 5) {
-            rndChar += abc[Math.floor(Math.random() * abc.length)];
-        }
-document.getElementById("randomValue").textContent = rndChar;
+let rndChar = "";
+const generateKey = () => {
+  rndChar = "";
+  while (rndChar.length < 5) {
+    rndChar += abc[Math.floor(Math.random() * abc.length)];
+  }
+  document.getElementById("randomValue").textContent = rndChar;
+};
 
-document.getElementById("btnSub").onclick = function(){
-    txtSub = document.getElementById("textBox").value;
-    if(txtSub==rndChar){
-        window.location.replace("/osint-helper/pages/transition.html");;
-    }
-    else{
-        window.alert("It DOESN'T WORKS");
-        console.log('hi');
-        location.reload();
-    }
-}
+document.getElementById("captchaForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const userInput = document.getElementById("textBox").value.trim();
 
-
-
+  if (userInput === rndChar) {
+    window.location.href = "/osint-helper/pages/transition.html";
+  } else {
+    alert("Incorrect value...");
+    generateCaptcha();
+    document.getElementById("textBox").value = "";
+  }
+  generateCaptcha();
+});
